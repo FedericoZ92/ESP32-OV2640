11382ea44e50268cce84ad5658e14cdb51395d83
Federico Zanetti <federico.zanetti@seitron.it>
2025-08-31 14:02:04 +0200

    feat: set up main to acquire a frame as example

--- main/main.cpp
+++ main/main.cpp
@@ -1,50 +1,50 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
 
 #include <stdio.h>
-#include <inttypes.h>
-#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
-#include "esp_system.h"
+#include "camera_driver.h"
 
-void app_main(void *pvParameters) 
+#define TAG "APP_MAIN"
+
+extern "C" void app_main(void)
 {
-    printf("Hello world!\n");
-
-    /* Print chip information */
+    // Print chip info
     esp_chip_info_t chip_info;
     uint32_t flash_size;
     esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+    printf("This is %s chip with %d CPU core(s)\n", CONFIG_IDF_TARGET, chip_info.cores);
+    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        printf("Get flash size failed\n");
+        return;
+    }
+    printf("Flash size: %" PRIu32 " MB\n", flash_size / (1024*1024));
 
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
+    // Camera
+    CameraDriver camera;
+    if (camera.init() != ESP_OK) {
+        ESP_LOGE(TAG, "Camera initialization failed");
         return;
     }
 
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    camera_fb_t* fb = camera.captureFrame();
+    if (fb) {
+        // Print first few bytes
+        for (int i = 0; i < 16 && i < fb->len; i++) {
+            printf("%02X ", fb->buf[i]);
+        }
+        printf("\n");
 
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+        camera.releaseFrame(fb);
+    }
 
+    // Restart countdown
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
     printf("Restarting now.\n");
     fflush(stdout);
